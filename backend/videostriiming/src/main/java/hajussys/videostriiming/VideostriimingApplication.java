18cc36b3cfdda43ba00597ea8eb5a5737e749c30
Johann <joveis@ttu.ee>
2020-05-08 14:10:36 +0300

    init

---
+++ backend/videostriiming/src/main/java/hajussys/videostriiming/VideostriimingApplication.java
@@ -0,0 +1,13 @@
+package hajussys.videostriiming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideostriimingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideostriimingApplication.class, args);
+    }
+
+}
---
+++ backend/videostriiming/src/test/java/hajussys/videostriiming/VideostriimingApplicationTests.java
@@ -0,0 +1,13 @@
+package hajussys.videostriiming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideostriimingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
